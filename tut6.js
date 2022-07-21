928de9d5aa0620f9abf02a9c2b85ae3d0fd905ac
abhisheknanda65-zessta <abhishek.nanda@zessta.com>
2022-07-21 15:52:30 +0530

    Add javascript join and reverse method

--- tut6.js
+++ tut6.js
@@ -41,4 +41,28 @@ console.log("We are listening from tut6 file");
   splitString(tempestString)
   splitString(monthString, comma)
 
+  /**
+   * Join() Method in Javascript:
+   * 
+   * The join() method creates and returns a new string by concatenating all of the elements in
+   * an array (or an array-like object), separated by commas or a specified separator string. I
+   *
+   * If the array has only one item, then that item will be returned without using the separator.
+   */
+
+   const elements = ['Fire', 'Air', 'Water'];
+
+   console.log(elements.join());
+   // expected output: "Fire,Air,Water"
+   
+   console.log(elements.join(''));
+   // expected output: "FireAirWater"
+   
+   console.log(elements.join('-'));
+   // expected output: "Fire-Air-Water"
+
+   console.log(elements.join('.'));
+   // expected output: "Fire.Air.Water"
+   
+
 
