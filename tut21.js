eb8b51c09cb0d77d262ce9068d363d95796dbf09
Abhishek Nanda <abhishek.nanda@zessta.com>
2023-01-11 13:26:50 +0530

    Add rest higher order function

--- tut21.js
+++ tut21.js
@@ -1,24 +1,47 @@
 const companies = [
-    { name: 'Google', category: 'product', start:1981, end:2004},
-    { name: 'Amazon', category: 'product', start:1991, end:2006},
-    { name: 'Paytm', category: 'product', start:1992, end:2008},
-    { name: 'Coforge', category: 'service', start:1989, end:2023},
-    { name: 'Mindtree', category: 'service', start:1990, end:2012},
+    { name: 'Google', category: 'product', start: 1981, end: 2004 },
+    { name: 'Amazon', category: 'product', start: 1991, end: 2006 },
+    { name: 'Paytm', category: 'product', start: 1992, end: 2008 },
+    { name: 'Coforge', category: 'service', start: 1989, end: 2023 },
+    { name: 'Mindtree', category: 'service', start: 1990, end: 2012 },
 ];
 
-const ages = [33, 12, 20, 16, 5, 54, 21,44,61,13,45];
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 45];
 
-companies.forEach(function(company, index){
-    // console.log(index);
-})
+companies.forEach(function (company, index) {
+    console.log(company);
+});
 
 // Filter
 
-ages.filter((age)=>{
-    if(age >20){
-        console.log(age);
+const filterAge = ages.filter((age) => {
+    if (age > 20) {
+        return age;
+    }
+});
 
-    }
-    }
-    )
-// console.log(filteredAge);
+console.log(filterAge);
+
+// Map
+
+const companiesWithCategory = companies.map((company) => {
+    return `${company.name} - ${company.category}`;
+});
+console.log(companiesWithCategory);
+
+// Sort
+const sortedComapny = companies.sort((c1, c2) =>
+    c1.start > c2.start ? 1 : -1
+);
+console.log(sortedComapny);
+
+const sortedDeAge = ages.sort((a, b) => b - a);
+console.log(sortedDeAge);
+
+const sortedInAge = ages.sort((a, b) => a - b);
+console.log(sortedInAge);
+
+// reduce:
+
+const redueImplementedAge = ages.reduce((total, age) => total + age, 0);
+console.log(redueImplementedAge);
