df2d96365cc7520640ee80b41e0565c941770fae
Abhishek Nanda <89909010+abhisheknanda65-zessta@users.noreply.github.com>
2023-02-21 23:14:53 +0530

    Debouching Function in Javascript

---
+++ tut22.js
@@ -0,0 +1,15 @@
+// Debouncing in Javascript
+
+function debounce(func, wait) {
+  let timeout;
+
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
